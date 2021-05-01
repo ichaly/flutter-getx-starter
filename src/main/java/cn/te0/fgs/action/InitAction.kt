b069c96d12af53d25660a2f41d39b20d582d68af
shi.changliang <shi.changliang@zhaopin.com.cn>
2021-05-02 06:18:27 +0800

    feat:添加依赖FlutterJsonBeanFactory项目

--- src/main/java/cn/te0/fgs/action/InitAction.kt
+++ src/main/java/cn/te0/fgs/action/InitAction.kt
@@ -2,8 +2,12 @@ package cn.te0.fgs.action
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.project.Project
+import com.ruiyu.helper.YamlHelper
+import com.ruiyu.utils.showNotify
 
-class InitAction : AnAction("JsonToDartBeanAction"){
+class InitAction : AnAction("JsonToDartBeanAction") {
 
     init {
         templatePresentation.apply {
@@ -13,6 +17,13 @@ class InitAction : AnAction("JsonToDartBeanAction"){
     }
 
     override fun actionPerformed(e: AnActionEvent) {
-        TODO("Not yet implemented")
+        var project: Project = e.getData(PlatformDataKeys.PROJECT)!!
+        val pubSpecConfig = YamlHelper.getPubSpecConfig(project)
+        //判断是否是flutter项目
+        if (YamlHelper.shouldActivateFor(project)) {
+            print("It is flutter project");
+        } else {
+            project.showNotify("This project is not the flutter project or the flutterJson in pubspec.yaml with the enable set to false")
+        }
     }
 }
